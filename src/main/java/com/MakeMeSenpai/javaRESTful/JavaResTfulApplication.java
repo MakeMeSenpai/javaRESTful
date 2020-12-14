290b080cd0b8cb48e3dcd9fe73c867d27c093b54
Anthony <sean.a.protho1@gmail.com>
2020-12-14 13:55:01 -0800

    spring initializer

---
+++ src/main/java/com/MakeMeSenpai/javaRESTful/JavaResTfulApplication.java
@@ -0,0 +1,13 @@
+package com.MakeMeSenpai.javaRESTful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaResTfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MakeMeSenpai/javaRESTful/JavaResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MakeMeSenpai.javaRESTful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaResTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
